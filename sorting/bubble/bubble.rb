73722b0b905f99cf02731c91763505a6478dc8df
fergmastaflex <privacy@notgettingmyemail.com>
2026-01-03 22:42:47 -0500

    Remove stdout

--- sorting/bubble/bubble.rb
+++ sorting/bubble/bubble.rb
@@ -1,10 +1,7 @@
 def sort(array)
   n = array.length - 1
-puts "Array is #{n} long"
   0.upto(n) do |i|
-    puts "Finding all items beyond #{i}"
     0.upto(n-i) do |j|
-      puts "j = #{j}"
       if array[j+1] != nil && array[j] > array[j+1]
         new_value = array[j]
         array[j] = array[j+1]
@@ -12,7 +9,7 @@ puts "Array is #{n} long"
       end
     end
   end
-  puts array
+  array
 end
 
 array = [9,2,0,1,11,42,102,1,9999]
